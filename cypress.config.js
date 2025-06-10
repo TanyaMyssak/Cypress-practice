2f5819bf940e0b0eb856ac49d751a0e30411f84e
TanyaMysak <tanyamyssak@gmail.com>
2025-06-10 15:12:11 +0200

    Working expenses

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   e2e: {
-    baseUrl: 'https://qauto.forstudy.space',
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
     
     setupNodeEvents(on, config) {
     },
--- cypress/e2e/fuelExpenses.cy.js
+++ cypress/e2e/fuelExpenses.cy.js
@@ -1,69 +1,25 @@
-
+/// <reference types="cypress" />
 import HomePage from "../Page_objects/Pages/HomePage";
 import SignInForm from "../Page_objects/Forms/SignInForm";
 import GaragePage from "../Page_objects/Pages/GaragePage";
-import ExpensesPage from "../Page_objects/Pages/FuelExpensesPage";
+import FuelExpensesPage from "../Page_objects/Pages/FuelExpensesPage";
 
-const garageCar = {
-  brand: 'Audi',
-  model: 'TT',
-  mileage: '723'
-};
+describe('Add Expense Modal', () => {
+  const addExpense = FuelExpensesPage;
 
-const testExpense = {
-  date: '2025-06-09',
-  mileage: 1240,
-  liters: 100,
-  cost: 1200
-};
+ beforeEach(() => {
+  HomePage.visit();
+  HomePage.openSignInForm();
+  SignInForm.loginWithCredentials(Cypress.env('TEST_USER_EMAIL'), Cypress.env('TEST_USER_PASSWORD'));
+  GaragePage.pageHeader.should('be.visible');
+/* Go to Expenses page */
+  cy.visit('/panel/expenses'); 
+});
 
-const fullName = `${garageCar.brand} ${garageCar.model}`;
+  it('should open the add expense modal, fill form and submit', () => {
+    addExpense.addExpense('Audi TT', '10.06.2025', '15000', '40', '70');
 
-describe('Should add Fuel Expenses for the added car', () => {
-    beforeEach(() => {
-        HomePage.visit();
-        HomePage.openSignInForm();
-        SignInForm.loginWithCredentials(Cypress.env('TEST_USER_EMAIL'), Cypress.env('TEST_USER_PASSWORD'));
-        GaragePage.pageHeader.should('be.visible');
-    })
-
-
-     it('Add [Audi] [TT] car', () => {
-            GaragePage.addNewCar('Audi', 'TT', '723');
-            GaragePage.verifyLastAddedCar('Audi TT');
-    
-        });
-    // Check if the car exists, if not add it
-    it. cy.get('body').then($body => {
-      if (!$body.text().includes(fullName)) {
-        GaragePage.addNewCar(garageCar.brand, garageCar.model, garageCar.mileage);
-        GaragePage.verifyLastAddedCar(fullName);
-      }
-    });
-
-    // Visit expenses page and verify it's loaded
-    ExpensesPage.visit();
-    ExpensesPage.verifyExpensesPageLoaded();
+    // Verify modal closed
+    cy.get('app-add-expense-modal').should('not.exist');
   });
-
-  it('should add fuel expense for a car', () => {
-    const vehicleName = fullName;
-
-    ExpensesPage.getExpensesCount().then(initialCount => {
-      ExpensesPage.addFuelExpense(
-        vehicleName,
-        testExpense.date,
-        testExpense.mileage,
-        testExpense.liters,
-        testExpense.cost
-      );
-
-      ExpensesPage.verifyExpenseExists(testExpense.cost);
-
-      ExpensesPage.getExpensesCount().then(newCount => {
-        expect(newCount).to.be.greaterThan(initialCount);
-      });
-
-      ExpensesPage.verifyModalClosed();
-    });
-  });
+});
